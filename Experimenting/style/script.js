858db1a9d1949a946c8640178d66d3047e1479a0
Barry Porch <bporch@ymail.com>
2020-12-10 19:54:14 -0500

    Added projects page to experimenting folder

--- Experimenting/style/script.js
+++ Experimenting/style/script.js
@@ -12,3 +12,23 @@ function removeActiveClasses() {
         panel.classList.remove("active")
     })
 }
+
+const boxes = document.querySelectorAll(".box")
+
+window.addEventListener("scroll", checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom){
+box.classList.add("show")
+        } else {
+            box.classList.remove("show")
+        }
+    })
+}
